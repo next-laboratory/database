1ea5d2f451ea659c64b51267131fb8aca2d805e0
chengyao <bigyao@139.com>
2021-08-29 00:30:33 +0800

    继承基础数据集类

--- src/Max/Database/History.php
+++ src/Max/Database/History.php
@@ -2,29 +2,8 @@
 
 namespace Max\Database;
 
-class History implements \IteratorAggregate
+class History extends \Max\Collection
 {
-
-    /**
-     * @var array
-     */
-    protected $items = [];
-
-    /**
-     * 记录历史
-     * @param $query
-     * @param $time
-     * @param $binds
-     */
-    public function record($query, $time, $boundParameters)
-    {
-        $this->items[] = compact(['query', 'time', 'boundParameters']);
-    }
-
-    public function getIterator()
-    {
-        return new \ArrayIterator($this->items);
-    }
 
     public function end()
     {
