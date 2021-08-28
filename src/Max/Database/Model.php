d96598c8f23f5d7ead9315faf74a6b87a75c6bda
chengyao <bigyao@139.com>
2021-08-29 00:31:09 +0800

    功能仍然不可用

--- src/Max/Database/Model.php
+++ src/Max/Database/Model.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Max\Database;
 
 use Max\App;
-use Max\Facade\Db;
+use Max\Facade\DB;
 
 /**
  * @method Driver where(array $where);
@@ -18,7 +18,7 @@ use Max\Facade\Db;
  * Class Model
  * @package Max
  */
-class Model
+class Model extends Collection
 {
 
     /**
@@ -38,6 +38,8 @@ class Model
      */
     protected $key = 'id';
 
+    protected $relations = [];
+
     /**
      * 数据集
      * @var \stdClass
@@ -50,141 +52,41 @@ class Model
      */
     public function __construct()
     {
-        $this->name       = $this->name ?? strtolower(ltrim(strrchr(get_called_class(), '\\'), '\\'));
-        $this->collection = new \stdClass();
+        $this->name = $this->name ?? strtolower(ltrim(strrchr(get_called_class(), '\\'), '\\'));
     }
 
-    public function beforeSave()
+    public function with($relations)
     {
+        $this->relations = (array)$relations;
+        return $this;
     }
 
-    protected function modifyAttr()
+    public function hasOne(string $model, $foreignKey = null, $key = null)
     {
-        $methods = get_class_methods(static::class);
-        foreach ($methods as $method) {
-            if ('withAttr' === substr($method, 0, 8)) {
-                $field                      = strtolower(substr($method, 8));
-                $this->collection->{$field} = $this->{$method}($this->collection->{$field});
-            }
-        }
-    }
-
-    /**
-     * 获取
-     */
-    public function getFi()
-    {
-        $methods = get_class_methods(static::class);
-        foreach ($methods as $method) {
-            if ('getAttr' === substr($method, 0, 8)) {
-                $field                      = strtolower(substr($method, 8));
-                $this->collection->{$field} = $this->{$method}($this->collection->{$field});
-            }
-        }
-    }
-
-    public function afterSave()
-    {
-    }
-
-    public function beforeDelete()
-    {
-    }
-
-    public function afterDelete()
-    {
-    }
-
-    public function beforeUpdate()
-    {
-    }
-
-    public function afterUpdate()
-    {
-    }
-
-    public function beforeSelect()
-    {
-    }
-
-    public function afterSelect()
-    {
-    }
-
-    public function save(array $data = [])
-    {
-        $this->setData($data);
-        return $this->trigger('Save', function () {
-            return $this->query->name($this->name)->insert($this->getData());
-        });
-    }
-
-    protected function getData(): array
-    {
-        return get_object_vars($this->collection);
-    }
-
-    protected function setData(array $data = [])
-    {
-        foreach ($data as $key => $value) {
-            $this->collection->{$key} = $value;
-        }
-    }
-
-    public function update(array $data, array $where = [])
-    {
-        $this->setData($data);
-        return $this->trigger('Update', function () use ($where) {
-            return $this->query->name($this->name)->where($where)->update($this->getData());
-        });
-    }
-
-    public function delete($where)
-    {
-        $this->trigger('Update', function () use ($where) {
-            return $this->query->name($this->name)->where($where)->delete();
-        });
-    }
-
-    public function trigger($event, \Closure $operation)
-    {
-        $event  = ucfirst($event);
-        $before = $this->{('before' . $event)}();
-        $this->modifyAttr();
-        $res = $operation($before);
-        $this->{('after' . $event)}();
-        return $res;
+        $foreignKey = $foreignKey ?? $model . '_id';
+        $key        = $this->name . '_id';
+        return app()->make($model)->where(["$foreignKey = $key"])->select();
     }
 
     public function select()
     {
-        $results = $this->query->name($this->name)->select();
-        foreach ($results as $result) {
-            if (is_array($result)) {
-
-            }
-        }
-
-
-        return $this->trigger('Select', function () {
-            return $this->query->name($this->name)->select();
-        });
+        $this->items = DB::name($this->name)->select()->toArray();
+        return $this;
     }
 
+    public function get()
+    {
+        foreach ($this->relations as $relation) {
+            $this->each(function ($item) use ($relation) {
+                $this->items[$relation] = $this->{$relation}();
+            });
+        }
+        return $this->items;
+    }
 
     final public function __call($method, $arguments)
     {
         return $this->query->name($this->name)->{$method}(...$arguments);
-    }
-
-    public function __get($field)
-    {
-        return $this->collection->{$field};
-    }
-
-    public function __set($field, $value)
-    {
-        $this->collection->{$field} = $value;
     }
 
     final public static function __setter(App $app)
