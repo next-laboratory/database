22156e83112635fa3752cff3935a1593d549407f
chengyao <bigyao@139.com>
2021-12-05 09:01:07 +0800

    去掉配置

--- src/Connectors/Connector.php
+++ src/Connectors/Connector.php
@@ -23,9 +23,9 @@ abstract class Connector implements ConnectorInterface
      * @var array
      */
     protected array $options = [
-        PDO::ATTR_CASE         => PDO::CASE_NATURAL,
-        PDO::ATTR_ERRMODE      => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL,
+        //        PDO::ATTR_CASE         => PDO::CASE_NATURAL,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        //        PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL,
         //        PDO::ATTR_STRINGIFY_FETCHES => false,
         //        PDO::ATTR_EMULATE_PREPARES  => false,
     ];
